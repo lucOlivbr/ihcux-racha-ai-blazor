f0b698a3d2f2ad3447cc127d3113db2ec993da45
lucOlivbr <buscadatorrenegra@gmail.com>
2026-05-10 17:32:15 -0300

    dashboard blazor

---
+++ Models/Grupo.cs
@@ -0,0 +1,9 @@
+namespace ihcux_racha_ai_blazor.Models;
+
+public class Grupo
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+    public decimal ValorPendente { get; set; }
+    public bool NoVermelho { get; set; }
+}
